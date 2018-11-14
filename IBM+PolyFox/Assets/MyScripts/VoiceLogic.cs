c84d30e19e3827c3d75279f05ae2dc589b4ec4c1
Samir Omarov <samir.omarov@gmail.com>
2018-11-15 01:20:23 +0300

    init

---
+++ IBM+PolyFox/Assets/MyScripts/VoiceLogic.cs
@@ -0,0 +1,43 @@
+﻿using MalbersAnimations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VoiceLogic : MonoBehaviour
+{
+    
+    public Button button;
+    public Animal animal;
+    // Use this for initialization
+
+    /*void Update()
+    {
+
+        button = GetComponent<Button>();
+    } */
+        public void FoxActions(string ActionCommands)
+    {
+
+        ActionCommands = ActionCommands.Trim();
+
+        if (ActionCommands == "sit")
+        {
+            //GetComponent<Button>().onClick.Invoke();
+            animal.SetAction(11);
+        }
+        else if (ActionCommands == "eat")
+            {
+                //GetComponent<Button>().onClick.Invoke();
+                animal.SetAction(2);
+            }
+
+
+    }
+    // Update is called once per frame
+
+
+
+}
+
+
